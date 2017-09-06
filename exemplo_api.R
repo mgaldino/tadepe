fa289db251e02cbd7c97f73c31d6944a33ccd42e
mgaldino <mcz.fea@gmail.com>
2017-09-06 18:07:11 -0300

    Criei duas funções: Uma gera urls, e outra para obter os jsons. Falta mais uma função para transformar em um único data.frame.

--- exemplo_api.R
+++ exemplo_api.R
@@ -5,6 +5,7 @@
 library(httr)
 library(jsonlite) 
 library(dplyr)
+library(stringr)
 
 # token
 token_meu <- "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiYXBpLXRlc3QifQ.vwzSUQKsig7JFQ2w-o85JgnVv-VytT2NsZYwRELTiaY"
@@ -27,59 +28,114 @@ df1 <- df[[1]] %>%
 
 View(df1)
 
+## gerando função
 ## paralisadas
-url <- "http://tadepe.transparencia.org.br/api/projects/content?"
-modifier0 <- 'status=paralyzed&type_of_work=construction&funded_by=city'
-modifier1 <- "&type_of_project="
-modifier2 <- "&status="
-
-type_project <- c("educational_space_1_room","educational_space_2_room",
-                  "educational_space_4_room", "educational_space_6_room",
-                  "educational_space_8_room","educational_space_10_room",
-                  "educational_space_1_room", "conventional_design_1",
-                  "conventional_design_2")
-
-status <- c("execution", "paralyzed","bidding", "hiring",
-            "in_recasting", "planning_by_the_proponent",
-            "unfinished")
 
 req <- list()
 aux_j <- list()
 aux_i <- list()
 n <- length(type_project)
 k <- length(status)
+
+
+gera_urls_app <- function() {
+  lista_urls <- vector("character", n*k)
+  
+  type_project <- c("educational_space_1_room","educational_space_2_room",
+                    "educational_space_4_room", "educational_space_6_room",
+                    "educational_space_8_room","educational_space_10_room",
+                    "educational_space_1_room", "conventional_design_1",
+                    "conventional_design_2")
+  
+  status <- c("execution", "paralyzed","bidding", "hiring",
+              "in_recasting", "planning_by_the_proponent",
+              "unfinished")
+  n <- length(type_project)
+  k <- length(status)
+  url <- "http://tadepe.transparencia.org.br/api/projects/content?"
+ 
+   modifier0 <- 'status=paralyzed&type_of_work=construction&funded_by=city'
+  modifier1 <- "&type_of_project="
+  modifier2 <- "&status="
+  
+  n <- length(type_project)
+  k <- length(status)
+  
+  for ( i in 1:n) {
+    for ( j in 1:k) {
+      lista_urls[j+(k*(i-1))] <- paste0(url, modifier0, modifier1, type_project[i],
+                        modifier2, status[j])
+    }
+    
+  }
+  return(lista_urls)
+
+}
+
+get_json <- function(sleep_time = .05) {
+  
+  stopifnot(require(jsonlite))
+  
+  url <- gera_urls_app()
+  n <- length(url)
+  
+  obras <- vector("list", n)
+  paginas <- vector("list", n)
+  
+  for ( i in 1:n) {
+    req <- GET(url[i], accept_json(),
+               add_headers(Authorization = token_meu))
+    
+    obras[[i]] <- fromJSON(content(req,type="text"))
+    
+    paginas[[i]] <- paralisadas$links
+    print(i)
+    Sys.sleep(sleep_time)
+  }
+  
+  return(list(obras, paginas))
+}
+
+
 lista_df <- vector("list", n)
 paginas <- list()
+i <- 1
 
-for (i in 1:n) {
-  contador <- 1
-  for (j in 1:k) {
-    url1 <- paste0(url, modifier0, modifier1, type_project[i],
-                   modifier2, status[j])
-   req <- GET(url1, accept_json(),
+while(finished != T) {
+    req <- GET(url[i], accept_json(),
                add_headers(Authorization = token_meu))
-   
+
    paralisadas <- fromJSON(content(req,type="text"))
    
-   # paginas[[j+(k*(i-1))]] <- paralisadas$links
-   # if(length(paginas[[j+(k*(i-1))]]) != 0) {
-   #   
-   # }
+   paginas[[i]] <- paralisadas$links
    
-   if(length(paralisadas[[1]])!=0) {
-     paralisadas1 <- paralisadas[[1]] %>%
+  if(length(paralisadas[[1]])!=0) {
+       paralisadas1 <- paralisadas[[1]] %>%
        as.data.frame
      
-     aux_j[[j]] <- paralisadas1[[3]]
-   } else {
-     contador <- contador - 1 # para que não fique elemento vazio na lista
-   }
+       lista_obras[[i]] <- paralisadas1[[3]]
+  }
 
+   bol_paginacao <- length(paginas[[i]]) != 0 # condição se tem paginação no resultado.
+   
+   if(bol_paginacao) {
+     j <- 2
+     string_tmp <- str_extract(paginas[[i]]$last, "page%5Bnumber%5D=[0-9]+")
+     last_page <- gsub("=", "", str_extract(string_tmp, "=[0-9]+"))
+     
+     while(j <= last_page) {
+       req <- GET(paste0(url[i], "&page=", j), accept_json(),
+                  add_headers(Authorization = token_meu))
+       
+       paralisadas <- fromJSON(content(req,type="text"))
+       paginas[[i]] <- paralisadas$links
+       j <- j+1
+       print(j)
+     }
+   }
+   
     print(paste(paste("i é", i), " e ", paste("j é", j)))
     contador <- contador + 1
-  }
-  aux_i[[i]] <- aux_j
-  lista_df[[i]] <- bind_rows(aux_i[[i]])
 }
 
 df_final <- bind_rows(lista_df)
